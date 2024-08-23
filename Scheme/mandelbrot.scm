acb36b63d9572bb78b3a3bc260120428ea660743
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2024-08-23 18:03:37 +0200

    Add mandelbrot implemented in scheme

--- Scheme/mandelbrot.scm
+++ Scheme/mandelbrot.scm
@@ -1,6 +1,34 @@
-(define (complex a b)
-	(cons a b))
-(define (real x)
-	(car x))
-(define (imag x)
-	(cdr x))
+;; Eigene Implementierung von iota
+(define (iota n)
+  (let loop ((i 0) (result '()))
+    (if (= i n)
+        (reverse result)
+        (loop (+ i 1) (cons i result)))))
+
+(define (mandelbrot c max-iter)
+  (define (mandelbrot-iter z iter)
+    (if (or (>= iter max-iter) (> (magnitude z) 2))
+        iter
+        (mandelbrot-iter (+ (* z z) c) (+ iter 1))))
+  (mandelbrot-iter 0 max-iter))
+
+(define (draw-mandelbrot width height x-min x-max y-min y-max max-iter)
+  (let* ((x-scale (/ (- x-max x-min) width))
+         (y-scale (/ (- y-max y-min) height)))
+    (for-each
+     (lambda (y)
+       (for-each
+        (lambda (x)
+          (let* ((real (+ x-min (* x-scale x)))
+                 (imag (+ y-min (* y-scale y)))
+                 (c (+ real (* imag 0+1i)))
+                 (m (mandelbrot c max-iter)))
+            (if (= m max-iter)
+                (display "#")
+                (display " "))))
+        (iota width))
+       (newline))
+     (iota height))))
+
+;; Beispielaufruf
+(draw-mandelbrot 80 40 -2.0 1.0 -1.0 1.0 100)
